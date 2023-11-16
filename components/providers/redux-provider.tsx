b0622c43f11f8e82b17dd5630da5ba5b136c3480
Sahil Verma <domainsahil007@gmail.com>
2023-11-17 01:45:19 +0530

    redux removed

--- components/providers/redux-provider.tsx
+++ components/providers/redux-provider.tsx
@@ -1,15 +1,15 @@
-import { store } from "@/redux/store";
-import React from "react";
-import { Provider } from "react-redux";
+// import { store } from "@/redux/store";
+// import React from "react";
+// import { Provider } from "react-redux";
 
-import { FC, ReactNode } from "react";
+// import { FC, ReactNode } from "react";
 
-interface Props {
-  children: ReactNode;
-}
+// interface Props {
+//   children: ReactNode;
+// }
 
-const ReduxProvider: FC<Props> = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
-};
+// const ReduxProvider: FC<Props> = ({ children }) => {
+//   return <Provider store={store}>{children}</Provider>;
+// };
 
-export default ReduxProvider;
+// export default ReduxProvider;
