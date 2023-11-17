3002a4c8d38ffdafcaecf42491df636729851dee
Sahil Verma <domainsahil007@gmail.com>
2023-11-17 14:56:14 +0530

    all icons added

--- components/custom/icons-grid.tsx
+++ components/custom/icons-grid.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
-// import { getIcons } from "virtual:react-icons-get-icons";
-
-import { motion } from "framer-motion";
-
-import * as data from "react-icons/ai";
-
-const icons = Object.keys(data).map((item, index) => ({
-  label: item,
-  // @ts-ignore
-  icon: data[item],
-}));
-
-import { child, parent } from "@/constants/variants";
 import {
   Tooltip,
   TooltipContent,
@@ -23,54 +10,70 @@ import {
 } from "../ui/tooltip";
 import SelectedIconSheet from "./selected-icon-sheet";
 
+import { data } from "@/constants/data";
+import { Skeleton } from "../ui/skeleton";
+
 interface Props {
   id: string;
 }
 
 const IconsGrid: FC<Props> = ({ id }) => {
+  const [icons, setIcons] = useState<
+    {
+      id: string;
+      label: string;
+      icon: any;
+    }[]
+  >([]);
+
   const [selectedIcon, setSelectedIcon] = useState<null | {
     id: string;
     label: string;
     icon: any;
   }>(null);
 
+  useEffect(() => {
+    // @ts-ignore
+    setIcons(data[id]);
+  }, []);
+
   return (
     <>
-      <motion.div
-        variants={parent}
-        initial="hidden"
-        animate="visible"
-        className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-12 gap-4 w-full"
-      >
+      <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-12 gap-4 w-full">
         <TooltipProvider>
-          {/* {Array.from({ length: 100 }).map((_, i) => ( */}
-          {icons.map((icon) => (
-            <Tooltip key={icon.label}>
-              <TooltipTrigger asChild>
-                <motion.button
-                  variants={child}
-                  onClick={() => {
-                    setSelectedIcon({
-                      icon: icon.icon,
-                      id,
-                      label: icon.label,
-                    });
-                  }}
-                  className="border rounded-lg aspect-square hover:bg-secondary transition-all w-full flex flex-col items-center justify-center text-center gap-2"
-                >
-                  <icon.icon className="text-2xl" />
-                  <p className="text-xs font-medium w-[60%] truncate">
-                    {icon.label}
-                  </p>
-                </motion.button>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>{icon.label}</p>
-              </TooltipContent>
-            </Tooltip>
-          ))}
+          {icons.length === 0
+            ? Array.from({ length: 50 }).map((_, i) => (
+                <Skeleton
+                  key={i}
+                  className="h-full w-full rounded-lg aspect-square"
+                />
+              ))
+            : icons.map((icon) => (
+                <Tooltip key={icon.label}>
+                  <TooltipTrigger asChild>
+                    <button
+                      onClick={() => {
+                        setSelectedIcon({
+                          icon: icon.icon,
+                          id,
+                          label: icon.label,
+                        });
+                      }}
+                      className="border rounded-lg aspect-square hover:bg-secondary transition-all w-full flex flex-col items-center justify-center text-center gap-2"
+                    >
+                      <icon.icon className="text-2xl" />
+                      <p className="text-xs font-medium w-[60%] truncate">
+                        {icon.label}
+                      </p>
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{icon.label}</p>
+                  </TooltipContent>
+                </Tooltip>
+              ))}
         </TooltipProvider>
-      </motion.div>
+      </div>
       <SelectedIconSheet
         selectedIcon={selectedIcon}
         setSelectedIcon={setSelectedIcon}
---
+++ constants/data.ts
@@ -0,0 +1,220 @@
+import * as ai from "react-icons/ai";
+import * as bs from "react-icons/bs";
+import * as bi from "react-icons/bi";
+import * as ci from "react-icons/ci";
+import * as cg from "react-icons/cg";
+import * as di from "react-icons/di";
+import * as fi from "react-icons/fi";
+import * as fc from "react-icons/fc";
+import * as fa from "react-icons/fa";
+import * as fa6 from "react-icons/fa6";
+import * as gi from "react-icons/gi";
+import * as go from "react-icons/go";
+import * as gr from "react-icons/gr";
+import * as hi from "react-icons/hi";
+import * as hi2 from "react-icons/hi2";
+import * as im from "react-icons/im";
+import * as lia from "react-icons/lia";
+import * as io from "react-icons/io";
+import * as io5 from "react-icons/io5";
+import * as lu from "react-icons/lu";
+import * as md from "react-icons/md";
+import * as pi from "react-icons/pi";
+import * as rx from "react-icons/rx";
+import * as ri from "react-icons/ri";
+import * as si from "react-icons/si";
+import * as sl from "react-icons/sl";
+import * as tb from "react-icons/tb";
+import * as tfi from "react-icons/tfi";
+import * as ti from "react-icons/ti";
+import * as vsc from "react-icons/vsc";
+import * as wi from "react-icons/wi";
+
+export const data = {
+  ai: Object.keys(ai).map((item) => ({
+    id: "ai",
+    label: item,
+    // @ts-ignore
+    icon: ai[item],
+  })),
+  bs: Object.keys(bs).map((item) => ({
+    id: "bs",
+    label: item,
+    // @ts-ignore
+    icon: bs[item],
+  })),
+  bi: Object.keys(bi).map((item) => ({
+    id: "bi",
+    label: item,
+    // @ts-ignore
+    icon: bi[item],
+  })),
+  ci: Object.keys(ci).map((item) => ({
+    id: "ci",
+    label: item,
+    // @ts-ignore
+    icon: ci[item],
+  })),
+  cg: Object.keys(cg).map((item) => ({
+    id: "cg",
+    label: item,
+    // @ts-ignore
+    icon: cg[item],
+  })),
+  di: Object.keys(di).map((item) => ({
+    id: "di",
+    label: item,
+    // @ts-ignore
+    icon: di[item],
+  })),
+  fi: Object.keys(fi).map((item) => ({
+    id: "fi",
+    label: item,
+    // @ts-ignore
+    icon: fi[item],
+  })),
+  fc: Object.keys(fc).map((item) => ({
+    id: "fc",
+    label: item,
+    // @ts-ignore
+    icon: fc[item],
+  })),
+  fa: Object.keys(fa).map((item) => ({
+    id: "fa",
+    label: item,
+    // @ts-ignore
+    icon: fa[item],
+  })),
+  fa6: Object.keys(fa6).map((item) => ({
+    id: "fa6",
+    label: item,
+    // @ts-ignore
+    icon: fa6[item],
+  })),
+  gi: Object.keys(gi).map((item) => ({
+    id: "gi",
+    label: item,
+    // @ts-ignore
+    icon: gi[item],
+  })),
+  go: Object.keys(go).map((item) => ({
+    id: "go",
+    label: item,
+    // @ts-ignore
+    icon: go[item],
+  })),
+  gr: Object.keys(gr).map((item) => ({
+    id: "gr",
+    label: item,
+    // @ts-ignore
+    icon: gr[item],
+  })),
+  hi: Object.keys(hi).map((item) => ({
+    id: "hi",
+    label: item,
+    // @ts-ignore
+    icon: hi[item],
+  })),
+  hi2: Object.keys(hi2).map((item) => ({
+    id: "hi2",
+    label: item,
+    // @ts-ignore
+    icon: hi2[item],
+  })),
+  im: Object.keys(im).map((item) => ({
+    id: "im",
+    label: item,
+    // @ts-ignore
+    icon: im[item],
+  })),
+  lia: Object.keys(lia).map((item) => ({
+    id: "lia",
+    label: item,
+    // @ts-ignore
+    icon: lia[item],
+  })),
+  io: Object.keys(io).map((item) => ({
+    id: "io",
+    label: item,
+    // @ts-ignore
+    icon: io[item],
+  })),
+  io5: Object.keys(io5).map((item) => ({
+    id: "io5",
+    label: item,
+    // @ts-ignore
+    icon: io5[item],
+  })),
+  lu: Object.keys(lu).map((item) => ({
+    id: "lu",
+    label: item,
+    // @ts-ignore
+    icon: lu[item],
+  })),
+  md: Object.keys(md).map((item) => ({
+    id: "md",
+    label: item,
+    // @ts-ignore
+    icon: md[item],
+  })),
+  pi: Object.keys(pi).map((item) => ({
+    id: "pi",
+    label: item,
+    // @ts-ignore
+    icon: pi[item],
+  })),
+  rx: Object.keys(rx).map((item) => ({
+    id: "rx",
+    label: item,
+    // @ts-ignore
+    icon: rx[item],
+  })),
+  ri: Object.keys(ri).map((item) => ({
+    id: "ri",
+    label: item,
+    // @ts-ignore
+    icon: ri[item],
+  })),
+  si: Object.keys(si).map((item) => ({
+    id: "si",
+    label: item,
+    // @ts-ignore
+    icon: si[item],
+  })),
+  sl: Object.keys(sl).map((item) => ({
+    id: "sl",
+    label: item,
+    // @ts-ignore
+    icon: sl[item],
+  })),
+  tb: Object.keys(tb).map((item) => ({
+    id: "tb",
+    label: item,
+    // @ts-ignore
+    icon: tb[item],
+  })),
+  tfi: Object.keys(tfi).map((item) => ({
+    id: "tfi",
+    label: item,
+    // @ts-ignore
+    icon: tfi[item],
+  })),
+  ti: Object.keys(ti).map((item) => ({
+    id: "ti",
+    label: item,
+    // @ts-ignore
+    icon: ti[item],
+  })),
+  vsc: Object.keys(vsc).map((item) => ({
+    id: "vsc",
+    label: item,
+    // @ts-ignore
+    icon: vsc[item],
+  })),
+  wi: Object.keys(wi).map((item) => ({
+    id: "wi",
+    label: item,
+    // @ts-ignore
+    icon: wi[item],
+  })),
+};
